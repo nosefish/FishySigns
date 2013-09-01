01090cc8c6814c20ae0e2d5747a58e97821081ad
Stefan Steinheimer <nosefish@minecraftonline.com>
2013-09-01 16:03:52 +0200

    added ChangeSignTextTask

---
+++ src/net/gmx/nosefish/fishysigns/task/common/ChangeSignTextTask.java
@@ -0,0 +1,88 @@
+package net.gmx.nosefish.fishysigns.task.common;
+
+import net.canarymod.api.world.World;
+import net.canarymod.api.world.blocks.Sign;
+import net.canarymod.api.world.blocks.TileEntity;
+import net.gmx.nosefish.fishylib.worldmath.FishyLocationInt;
+import net.gmx.nosefish.fishysigns.task.FishyTask;
+
+/**
+ * Changes the text on a sign to the one passed in the constructor.
+ * 
+ * @author Stefan Steinheimer (nosefish)
+ *
+ */
+public class ChangeSignTextTask extends FishyTask {
+	private static final int NUM_LINES = 4;
+	private static final int LINE_LENGTH = 15;
+	
+	private final FishyLocationInt signLoc;
+	private final String[] newText;
+	private final boolean sendPacket;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param signLocation
+	 *     the location of the sign to change.
+	 *     
+	 * @param newText
+	 *     the text to write on the sign. Must have length 4! <code>null</code> lines will be ignored
+	 *     
+	 * @param sendPacket
+	 *     whether or not to notify players of the sign update
+	 */
+	public ChangeSignTextTask(FishyLocationInt signLocation, String[] newText, boolean sendPacket) {
+		this.signLoc = signLocation;
+		this.newText = newText;
+		this.sendPacket = sendPacket;
+		if (newText.length != NUM_LINES) {
+			throw new InvalidSignTextException("Invalid number of lines, expected " + 
+			                                    NUM_LINES +
+			                                    ", but received " + 
+			                                    newText.length);
+		}
+	}
+
+	@Override
+	public void doStuff() {
+		World world = signLoc.getWorld().getWorldIfLoaded();
+		if (world == null) {
+			return;
+		}
+		TileEntity te = world.getOnlyTileEntityAt(signLoc.getIntX(), signLoc.getIntY(), signLoc.getIntZ());
+		if ((te != null) && (te instanceof Sign)) {
+			Sign sign = (Sign) te;
+			for (int line = 0; line < NUM_LINES; ++line){
+				if (newText[line] != null) {
+					if (newText[line].length() > LINE_LENGTH) {
+						throw new InvalidSignTextException("Line too long: '" + 
+                                newText[line] +
+                                "' has " + 
+                                newText.length +
+                                " characters, but only " +
+                                LINE_LENGTH +
+                                " are allowed on a sign.");
+					}
+				}
+				sign.setTextOnLine(newText[line], line);
+			}
+			if (sendPacket) {
+				sign.update();
+			}
+		}
+	}
+	
+	public class InvalidSignTextException extends RuntimeException {
+		private static final long serialVersionUID = -1342781346250422909L;
+
+		public InvalidSignTextException() {
+			super();
+		}
+		
+		public InvalidSignTextException(String message) {
+			super(message);
+		}
+	}
+
+}
