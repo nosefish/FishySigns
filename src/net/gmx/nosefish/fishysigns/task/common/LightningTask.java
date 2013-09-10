2e2f2cb07f3fed96b073dc724b493e13fe9795bf
nosefish <nosefish@gmx.net>
2013-09-10 14:15:32 +0200

    added FishyTasks to make lightning

---
+++ src/net/gmx/nosefish/fishysigns/task/common/LightningTask.java
@@ -0,0 +1,27 @@
+package net.gmx.nosefish.fishysigns.task.common;
+
+import net.canarymod.api.world.World;
+import net.gmx.nosefish.fishylib.worldmath.FishyLocationInt;
+import net.gmx.nosefish.fishysigns.task.FishyTask;
+
+/**
+ * Strikes the specified location with lightning
+ * 
+ * @author Stefan Steinheimer (nosefish)
+ *
+ */
+public class LightningTask extends FishyTask {
+	protected FishyLocationInt location;
+	
+	public LightningTask(FishyLocationInt location) {
+		this.location = location;
+	}
+
+	@Override
+	public void doStuff() {
+		World world = location.getWorld().getWorldIfLoaded();
+		if (world != null) {
+			world.makeLightningBolt(location.getIntX(), location.getIntY(), location.getIntZ());
+		}
+	}
+}
